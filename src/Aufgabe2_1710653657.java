89f55287dadafd2a47cf38b91c1ab445e54d2e64
kilianbauer <1710653657@stud.fh-kufstein.ac.at>
2018-11-22 07:49:19 +0100

    Aufgabe2

---
+++ src/Aufgabe2_1710653657.java
@@ -0,0 +1,43 @@
+public class Aufgabe2_1710653657 {
+
+    public static void main(String[]args){
+
+        //Zeichenkette
+
+        String name = "Kilian";
+
+        //Natürliche Zahlen von -2.147.483.648 ... 2.147.483.647
+
+        int number = 15;
+
+        // +/-4,9E-324 ... +/-1,7E+308
+
+        double comma = 17.52;
+
+        // true  false
+
+        boolean wahr = true;
+
+        // Natürliche Zahlen von -2^63 bis 2^63-1, ab Java 8 auch 0 bis 2^64 -1
+
+        long longnumber = 123345456;
+
+        // Buchstaben
+
+        char letter = 'b';
+
+        // -2^7 .. 2^7 -1 entspricht -128 ... 127
+
+        byte shortnumber = 64;
+
+        // -32768 ... 32767
+
+        short number2 = 2356;
+
+        //+/-1,4E-45 ... +/-3,4E+38
+
+        float comma2 = 2135242;
+
+
+    }
+}

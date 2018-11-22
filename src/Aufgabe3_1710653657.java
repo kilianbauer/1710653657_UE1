b6d4582b2e634d12eedd5619bf45a7e07d0db000
kilianbauer <1710653657@stud.fh-kufstein.ac.at>
2018-11-22 08:04:42 +0100

    Aufgabe3

---
+++ src/Aufgabe3_1710653657.java
@@ -0,0 +1,22 @@
+public class Aufgabe3_1710653657 {
+
+    public static void main (String[]args){
+
+        int zahl1 = 7;
+        int zahl2 = 4;
+        int zahl3 = 11;
+        int zahl4= 8;
+
+        System.out.println( zahl1 + zahl2);
+
+        System.out.println( zahl1 / zahl3);
+
+        System.out.println( zahl4 * zahl2);
+
+        System.out.println( zahl1 % zahl2);
+
+        System.out.println( zahl3++);
+
+        System.out.println( zahl2--);
+    }
+}

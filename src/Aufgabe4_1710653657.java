dc98ae2d2f801a2ac8ef019a1954481d7bf51651
kilianbauer <1710653657@stud.fh-kufstein.ac.at>
2018-11-22 08:13:27 +0100

    Aufgabe4

---
+++ src/Aufgabe4_1710653657.java
@@ -0,0 +1,28 @@
+public class Aufgabe4_1710653657 {
+
+    public static void main (String[]args){
+
+        int zahl1 = 8;
+        int zahl2 = 11;
+
+        System.out.println(zahl1 < zahl2);
+
+        System.out.println(zahl2 < zahl1);
+
+        System.out.println(zahl1 > zahl2);
+
+        System.out.println(zahl2 > zahl1);
+
+        System.out.println(zahl1 <= zahl2);
+
+        System.out.println(zahl2 <= zahl1);
+
+        System.out.println(zahl1 >= zahl2);
+
+        System.out.println(zahl2 >= zahl1);
+
+        System.out.println(zahl1 == zahl2);
+
+        System.out.println(zahl2 != zahl1);
+    }
+}
